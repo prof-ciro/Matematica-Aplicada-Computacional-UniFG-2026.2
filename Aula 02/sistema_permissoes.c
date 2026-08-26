487d1b84b68960b0c88bf53e71d20775dad613b3
Ciro Chaves <ciro.chaves@ulife.com.br>
2026-08-26 00:37:42 +0000

    Atualização do Sistema de Permissões da aula 2 para refletir exemplos em sala.

--- Aula 02/sistema_permissoes.c
+++ Aula 02/sistema_permissoes.c
@@ -1,24 +1,18 @@
 //System permission
-#define READ 4
-#define WRITE 2
-#define EXECUTE 1
-
-//Common permissions
-#define PERM_RW 6
-#define PERM_RX 5
-#define PERM_WX 3
-#define PERM_RWX 7
+#define READ 4 //0b100 ou 0x04
+#define WRITE 2 //0b010 ou 0x02
+#define EXECUTE 1 //0b001 ou 0x01
 
 #include<stdio.h>
 
 //Add permission function
 void add_perm(unsigned int *perm, unsigned int p){
-    *perm |= p;
+    *perm |= p; //Example: perm = 0b011 OR 0b100 = 0b111
 }
 
 //Remove permission function
 void remove_perm(unsigned int *perm, unsigned int p){
-    *perm &= ~p;
+    *perm &= ~p; //perm = 0b111 AND ~(WRITE) => 0b111 AND ~(010) => 0b111 AND 101 = 0b101
 }
 
 //Check user permission
@@ -33,7 +27,8 @@ void print_status(unsigned int perm){
 }
 
 int main(){
-    unsigned int user_perm = WRITE | EXECUTE; //No permission
+    unsigned int user_perm = WRITE | EXECUTE; //WRITE OR EXECUTE = 0b010 OR 0b001 = 0b011
     add_perm(&user_perm,READ); //Add READ permission
+    remove_perm(&user_perm,WRITE); //Remove WRITE permission
     print_status(user_perm); //Print active permissions
 }
